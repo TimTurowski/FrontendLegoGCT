1df70d30f2785f7b7a6269a83eff4c89eb9525e2
Tim Sibum <tim.sibum@studmail.w-hs.de>
2023-12-10 14:22:48 +0100

    Details für die Liste hinzufügen

--- FrontendLegoGCT/src/app/liste/details/details.component.ts
+++ FrontendLegoGCT/src/app/liste/details/details.component.ts
@@ -3,24 +3,71 @@ import {ApiService} from "../../api.service";
 import {CookieService} from "ngx-cookie-service";
 import {Router} from "@angular/router";
 import {Shop} from "../../suche/datenstrukturen";
+import {SucheComponent} from "../../suche/suche.component";
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+    selector: 'app-details',
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent{
-  @Input() legoSet: any;
-  @Input() legoSetDetails: Shop[] = [];
-  constructor(
-    private apiService: ApiService,
+export class DetailsComponent {
+    @Input() legoSet: any;
+    @Input() legoSetDetails: Shop[] = [];
+    bilder: Map<string, string> = new Map();
 
-  ) {}
+    constructor(
+        private router: Router,
+        private apiService: ApiService,
+        private suche: SucheComponent
+    ) {
+    }
+
+    /**
+     * gibt zu einer Set Id das entsprechende Bild
+     * @param set_id übergebene Set Id
+     */
+    getSetBild(set_id: string) {
+        let bild: string = "";
+        // prüft, ob das Bild bereits geladen wurde
+        if (!this.bilder.has(set_id)) {
+            //läd Bild aus der Datenbank
+            this.suche.getBild(set_id).subscribe(data => {
+
+                const parsed_data = JSON.parse(JSON.stringify(data));
+                this.bilder.set(set_id, parsed_data.set_bild);
+            });
+        } else {
+            //holt das Bild aus dem Cache
+            // @ts-ignore
+            bild = this.bilder.get(set_id);
+        }
+        return bild;
+
+    }
+
+    /**
+     * erzeugt eine Textuelle Darstellung des Datums
+     * @param raw_date Json darstellung des Suchzeitpunkts
+     */
+    getSucheDate(raw_date:string) {
+        const date:Date = new Date(raw_date);
+        return date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
+    }
+
+    /**
+     * erzeugt eine Textuelle Darstellung der Suchuhrzeit
+     * @param raw_date Json darstellung des Suchzeitpunkts
+     */
+    getSucheTime(raw_date:string) {
+        const date:Date = new Date(raw_date);
+        return date.getHours() + ":" + date.getMinutes();
+    }
 
 
 
 
-  protected readonly console = console;
+
+    protected readonly console = console;
 
 
 }
